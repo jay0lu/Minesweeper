916ec2470e2c153643bdc7b9f7d72ebeba636e99
jay0lu <lzhen009@gmail.com>
2019-03-01 20:20:55 -0500

    init mine map

--- map/models.py
+++ map/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from random import randint
 
-col = 10
-row = 10
-mine = 10
-mineLocation = []
-def generateMap():
+def generateMap(col, row, mine):
+  col = col
+  row = row
+  mine = mine
+
+  col = 10
+  row = 10
+  mine = 10
+
   limit = col * row
+
+  mineLocation = generateMines(mine, limit)
+
+# random generate mines
+def generateMines(mine, limit):
+  mineLocation = []
+
   i = 0
   while i < mine:
     location = randint(0, limit)
@@ -14,7 +25,26 @@ def generateMap():
       mineLocation.append(location)
       i += 1
 
+  mineLocation.sort()
+  mineInsertMap = insertMine(mineLocation)
+  return mineInsertMap
+
+# According to the mineLocation, generate map
+def insertMine(list):
+  i = 0
+  board = []
+  for i in limit:
+    if i in list:
+      # 9 indicate
+      board.append(9)
+    else:
+      board.append(0)
+  return board
+
+# Adding 1 to the mine surroundings
+def plus(list):
+  
 
 class map(models.Model):
-  uid = models.CharField(max_length=300, unique=True)
-  mine = models.CharField()
+  uid = models.CharField(max_length=50, unique=True)
+  mineMap = models.CharField(max_length=limit)
