1f1c9019aa814e61f4bd3a7eeee82a3fe64b0675
jay0lu <lzhen009@gmail.com>
2019-03-01 20:21:04 -0500

    Bug fix

--- map/models.py
+++ map/models.py
@@ -43,7 +43,38 @@ def insertMine(list):
 
 # Adding 1 to the mine surroundings
 def plus(list):
-  
+  for i in range(0, len(list)):
+    if list[i] == 9:
+      # add 1 to up and down
+      if (i-col) >= 0:
+        if list[i-col] != 9:
+          list[i-col] += 1
+      if (i + col) <= len(list):
+        if list[i+col] != 9:
+          list[i+col] += 1
+      # check left
+      if (i % col) > 0:
+        if (i-1) >= 0:
+          if list[i-1] != 9:
+            list[i-1] += 1
+        if (i-col-1) >= 0:
+          if list[i-col-1] != 9:
+            list[i-col-1] += 1
+        if (i+col-1) <= len(list):
+          if list[i+col-1] != 9:
+            list[i+col-1] += 1
+      # check right
+      if (i % col) != (col - 1):
+        if (i + 1) <= len(list):
+          if list[i+1] != 9:
+            list[i+1] += 1
+        if (i-col+1) >= 0:
+          if list[i-col+1] != 9:
+            list[i-col+1] += 1
+        if (i+col+1) <= len(list):
+          if list[i+col+1] != 9:
+            list[i+col+1] += 1
+
 
 class map(models.Model):
   uid = models.CharField(max_length=50, unique=True)
